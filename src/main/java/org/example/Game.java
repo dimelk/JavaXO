3b969c43ea00156fba6baa4bc1f2a51a19e2a102
dimelk <adelshin.dima@gmail.com>
2025-12-19 20:38:36 +0600

    first version of field 3 by 3(with ending a game)

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,11 +1,49 @@
 package org.example;
 public class Game {
 
+    private static int sumCrossX = 0;
+    private static int sumCrossY = 0;
+
+    private static int sumZeroX = 0;
+    private static int sumZeroY = 0;
+
+    private static boolean isEnded = false;
+
+    private static int moves = 0;
+
     public static final char[] symbols = {'X', '0'};
-    public static boolean IsEnd(char[][] Table) {
-        boolean isEnded = false;
+    public static boolean isEnded() {
+
+        if (moves > 4) {
+            if ((sumCrossX % 3 == 0) || (sumCrossY % 3 == 0) || (sumZeroX % 3 == 0) || (sumZeroY % 3 == 0)) {
+                isEnded = true;
+            }
+        }
 
         if (isEnded) return true;
         else return false;
+    }
+    public static void getCrossX(int x) {
+        sumCrossX += x;
+    }
+
+    public static void getCrossY(int y) {
+        sumCrossY += y;
+    }
+
+    public static void getZeroX(int x) {
+        sumZeroX += x;
+    }
+
+    public static void getZeroY(int y) {
+        sumZeroY += y;
+    }
+
+    public static boolean getIsEnded() {
+        return isEnded;
+    }
+
+    public static void incrMoves() {
+        moves++;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import java.util.Scanner;
-import static org.example.Game.IsEnd;
+import static org.example.Game.isEnded;
 import static org.example.Table.DrawTable;
 
 public class Main {
@@ -14,28 +14,38 @@ public class Main {
                 field.getTable()[p][q] = ' ';
             }
         }
+        int moves = 0;
+        while (!Game.getIsEnded()) {
 
-        for (int i = 0; i < 9; i++) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("X move: ");
             X x = new X();
             int x0 = scanner.nextInt();
             int y0 = scanner.nextInt();
+            Game.getCrossX(x0);
+            Game.getCrossY(y0);
             x.getCoordinates(x0,y0);
             field.WriteX(x);
             field.getTable()[x.getX()][x.getY()] = 'X';
-
+            Game.incrMoves();
             DrawTable();
+
+            if (Game.isEnded()) break;
 
             System.out.print("O move: ");
             O o = new O();
             int x1 = scanner.nextInt();
             int y1 = scanner.nextInt();
+            Game.getZeroX(x1);
+            Game.getZeroY(y1);
             o.getCoordinates(x1,y1);
             field.WriteO(o);
             field.getTable()[o.getX()][o.getY()] = 'O';
-
+            Game.incrMoves();
             DrawTable();
+
+            if (Game.isEnded()) break;
         }
+        System.out.println("game ended");
     }
 }
